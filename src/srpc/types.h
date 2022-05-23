0314a266392406581ef433ec211366db9c28f7e0
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-05-23 12:27:57 +0200

    Add startup store and load struct types for error handling

--- src/srpc/types.h
+++ src/srpc/types.h
@@ -11,6 +11,8 @@
 typedef struct srpc_module_change_s srpc_module_change_t;
 typedef struct srpc_operational_s srpc_operational_t;
 typedef struct srpc_rpc_s srpc_rpc_t;
+typedef struct srpc_startup_load_s srpc_startup_load_t;
+typedef struct srpc_startup_store_s srpc_startup_store_t;
 
 /**
  * Struct used to gather all module change callbacks based on a path.
@@ -51,5 +53,23 @@ typedef int (*srpc_startup_store_cb)(void *priv, const struct lyd_node *parent_n
 /** Callback type for applying changes when using sr_get_change_tree_next() functionality. */
 typedef int (*srpc_change_cb)(void *priv, sr_session_ctx_t *session, const char *prev_value,
                               const struct lyd_node *node, sr_change_oper_t operation);
+
+/**
+ * Used mostly for error messages with the provided callback.
+ */
+struct srpc_startup_store_s
+{
+    const char *name;         ///< Name of the value for which the callback is being called.
+    srpc_startup_store_cb cb; ///< Store callback.
+};
+
+/**
+ * Used mostly for error messages with the provided callback.
+ */
+struct srpc_startup_load_s
+{
+    const char *name;        ///< Name of the value for which the callback is being called.
+    srpc_startup_load_cb cb; ///< Load callback.
+};
 
 #endif // SRPC_TYPES_H
