3f28f9d27f5f5b9a89fb06e1b11e32b4248d9206
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-06-11 13:39:07 +0200

    Add changes iterator functionality

--- src/srpc/common.c
+++ src/srpc/common.c
@@ -41,3 +41,49 @@ error_out:
 out:
     return error;
 }
+
+/**
+ * Iterate changes for the provided xpath and use callback on each change.
+ *
+ * @param priv Private user data - pass plugin context.
+ * @param session Sysrepo session to use for iteration.
+ * @param xpath XPath for the changes iterator.
+ * @param cb Callback to call on each change.
+ *
+ * @return Error code - 0 on success.
+ */
+int srpc_iterate_changes(void *priv, sr_session_ctx_t *session, const char *xpath, srpc_change_cb cb)
+{
+    int error = 0;
+
+    // sysrepo
+    sr_change_iter_t *changes_iterator = NULL;
+    sr_change_oper_t operation = SR_OP_CREATED;
+    const char *prev_value = NULL, *prev_list = NULL;
+    int prev_default;
+
+    // libyang
+    const struct lyd_node *node = NULL;
+
+    error = sr_get_changes_iter(session, xpath, &changes_iterator);
+    if (error != SR_ERR_OK)
+    {
+        return -1;
+    }
+
+    int counter = 2;
+
+    while (sr_get_change_tree_next(session, changes_iterator, &operation, &node, &prev_value, &prev_list,
+                                   &prev_default) == SR_ERR_OK)
+    {
+        error = cb(priv, session, prev_value, node, operation);
+        if (error)
+        {
+            // return number of invalid callback
+            return -counter;
+        }
+        ++counter;
+    }
+
+    return 0;
+}
--- src/srpc/common.h
+++ src/srpc/common.h
@@ -5,6 +5,8 @@
 #ifndef SRPC_COMMON_H
 #define SRPC_COMMON_H
 
+#include "types.h"
+
 #include <stdbool.h>
 #include <sysrepo_types.h>
 
@@ -18,5 +20,17 @@
  * @return Error code - 0 on success.
  */
 int srpc_check_empty_datastore(sr_session_ctx_t *session, const char *path, bool *empty);
+
+/**
+ * Iterate changes for the provided xpath and use callback on each change.
+ *
+ * @param priv Private user data - pass plugin context.
+ * @param session Sysrepo session to use for iteration.
+ * @param xpath XPath for the changes iterator.
+ * @param cb Callback to call on each change.
+ *
+ * @return Error code - 0 on success.
+ */
+int srpc_iterate_changes(void *priv, sr_session_ctx_t *session, const char *xpath, srpc_change_cb cb);
 
 #endif // SRPC_COMMON_H
