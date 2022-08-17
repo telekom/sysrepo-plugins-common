a731884d96a3f5a0decee6615e37e2ee54e2961e
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-08-18 00:12:14 +0200

    Add initial feature status test file

--- test/Tests.cmake
+++ test/Tests.cmake
@@ -1,3 +1,4 @@
+# ly_tree
 add_executable(
 	test_ly_tree
 
@@ -14,3 +15,21 @@ target_link_libraries(
 )
 
 add_test(NAME test_ly_tree COMMAND test_ly_tree)
+
+# feature_status
+add_executable(
+	test_feature_status
+
+	test/test_feature_status.c
+)
+
+target_link_libraries(
+	test_feature_status
+
+	${CMOCKA_LIBRARIES}
+	${SYSREPO_LIBRARIES}
+	${LIBYANG_LIBRARIES}
+	${CMAKE_PROJECT_NAME}
+)
+
+add_test(NAME test_feature_status COMMAND test_feature_status)
---
+++ test/test_feature_status.c
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2022 Deutsche Telekom AG.
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include <srpc.h>
+
+static void test_feature_status(void **state);
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_feature_status),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
+
+static void test_feature_status(void **state)
+{
+    (void)state;
+}
